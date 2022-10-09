3984970b506f5b4854fa0d469c1951af21cff3b5
Johan Smit <jscjohansmit@gmail.com>
2022-10-09 10:34:07 +0200

    Added Background Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BackgroundComponent } from './background/background.component';
+import { BackgroundComponent } from './components/background/background.component';
 
 @NgModule({
   declarations: [
